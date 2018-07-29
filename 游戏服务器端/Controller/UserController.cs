725847a1a30ed2d5eaeb6a6417a077317f14225d
XiBinBin <1290212854@qq.com>
2018-07-29 11:24:25 +0800

    遗漏的上传

---
+++ 游戏服务器端/Controller/UserController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+using GameServer.Servers;
+using GameServer.Model;
+using GameServer.DAO;
+
+namespace GameServer.Controller
+{
+    class UserController:BaseController
+    {
+        private UserDAO userDAO = new UserDAO();
+        public UserController()
+        {
+            requestCode = RequestCode.User;
+        }
+        public string Login(string data,Client client)
+        {
+            string[] strs = data.Split(',');
+            User user = userDAO.VerifyUser(client.MysqlConn,strs[0],strs[1]);
+            if (user == null)
+            {
+                return ((int)ReturnCode.Fail).ToString();
+            }
+            else
+            {
+                return ((int)ReturnCode.Success).ToString();
+            }
+        }
+        public string Register(string data,Client client)
+        {
+            string[] strs = data.Split(',');
+            string username = strs[0];string password = strs[1];
+            bool res = userDAO.GetUserByUsername(client.MysqlConn, username);
+            if (res)
+            {
+                return ((int)ReturnCode.Fail).ToString();
+            }
+            userDAO.AddUser(client.MysqlConn, username,password);
+            return ((int)ReturnCode.Success).ToString();
+        }
+    }
+}
---
+++ 游戏服务器端/DAO/UserDAO.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameServer.Model;
+using MySql.Data.MySqlClient;
+
+namespace GameServer.DAO
+{
+    class UserDAO
+    {
+        public User VerifyUser(MySqlConnection mySqlConn,string username,string password)
+        {
+            MySqlDataReader reader = null;
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("select * from User where username=@username and password=@password",mySqlConn);
+                cmd.Parameters.AddWithValue("username", username);
+                cmd.Parameters.AddWithValue("password", password);
+                reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    int id = reader.GetInt32("id");
+                    User user = new User(id ,username,password);
+                    return user;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("在VerifyUser的时候出现异常"+e);
+            }
+            finally
+            {
+                if (reader != null) reader.Close();
+            }
+            return null;
+        }
+        public bool GetUserByUsername(MySqlConnection mysqlConn,string username)
+        {
+            MySqlDataReader reader = null;
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("select * from User where username=@username",mysqlConn);
+                cmd.Parameters.AddWithValue("username", username);
+                reader = cmd.ExecuteReader();
+                return reader.HasRows;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("在GetUserByUsername时候出现异常"+e);
+            }
+            finally
+            {
+                if (reader != null) reader.Close();
+            }
+            return false;
+        }
+        public void AddUser(MySqlConnection mySqlConn, string username,string password)
+        {
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("insert into User set username=@username , password=@password",mySqlConn);
+                cmd.Parameters.AddWithValue("username", username);
+                cmd.Parameters.AddWithValue("password", password);
+                cmd.ExecuteNonQuery();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("在AddUser的时候出现异常"+e);
+            }
+        }
+    }
+}
